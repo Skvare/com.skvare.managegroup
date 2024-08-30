fc95511a31da9e5b020db16408b4f8d7aa66491a
Sunil Pawar <sunil@skvare.com>
2024-08-30 19:57:54 +0530

    civix upgrade for smarty

--- CRM/Managegroup/Upgrader.php
+++ CRM/Managegroup/Upgrader.php
@@ -4,7 +4,7 @@ use CRM_Managegroup_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Managegroup_Upgrader extends CRM_Managegroup_Upgrader_Base {
+class CRM_Managegroup_Upgrader extends CRM_Extension_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
--- managegroup.php
+++ managegroup.php
@@ -15,15 +15,6 @@ function managegroup_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function managegroup_civicrm_xmlMenu(&$files) {
-  _managegroup_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -33,92 +24,12 @@ function managegroup_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function managegroup_civicrm_postInstall() {
-  _managegroup_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function managegroup_civicrm_uninstall() {
-  _managegroup_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function managegroup_civicrm_enable() {
   _managegroup_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function managegroup_civicrm_disable() {
-  _managegroup_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function managegroup_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _managegroup_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function managegroup_civicrm_managed(&$entities) {
-  _managegroup_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Add CiviCase types provided by this extension.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function managegroup_civicrm_caseTypes(&$caseTypes) {
-  _managegroup_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Add Angular modules provided by this extension.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function managegroup_civicrm_angularModules(&$angularModules) {
-  // Auto-add module files from ./ang/*.ang.php
-  _managegroup_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function managegroup_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _managegroup_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -164,14 +75,6 @@ function managegroup_civicrm_entityTypes(&$entityTypes) {
       ],
     ];
   };
-  _managegroup_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_themes().
- */
-function managegroup_civicrm_themes(&$themes) {
-  _managegroup_civix_civicrm_themes($themes);
 }
 
 function managegroup_civicrm_buildForm($formName, &$form) {
